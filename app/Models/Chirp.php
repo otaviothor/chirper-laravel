8462da4ea0f8d1a6e260972ffe7a629e03c50a3b
Otávio Silva <otaviosilva2632@gmail.com>
2025-01-06 17:38:29 -0300

    feat: showing chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
@@ -9,4 +10,9 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
